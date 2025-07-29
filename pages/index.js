e6da4f67ca94a6025cedc0e6f5104a64d7e96de6
Gabriel Porto <70228491+p0rto@users.noreply.github.com>
2025-07-29 02:42:31 +0000

    add index page

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function Home() {
+    return <div style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      textAlign: 'center',
+    }}>
+        <h1>Júlia & Gabriel</h1>
+        <h2>09.09.16 - ♾️</h2>
+        <img src="https://i.redd.it/wrgxqesjz8ze1.gif" 
+        style={{ width: '400px', height: 'auto' }}/>
+        <span>Te amo ontem, hoje e amanhã, guriazinha.</span>
+        </div>
+}
+
+export default Home;
