7bbb08c10904dfb29943981589688fd354d31f1b
Gabriel Porto <70228491+p0rto@users.noreply.github.com>
2025-09-06 04:01:05 +0000

    feat: create and use `ValidationError` in `controller.js`

--- infra/controller.js
+++ infra/controller.js
@@ -1,4 +1,8 @@
-const { MethodNotAllowedError, InternalServerError } = require("./errors");
+import {
+  InternalServerError,
+  MethodNotAllowedError,
+  ValidationError,
+} from "./errors.js";
 
 function onNoMatchHandler(request, response) {
   const publicErrorObject = new MethodNotAllowedError();
@@ -6,6 +10,11 @@ function onNoMatchHandler(request, response) {
 }
 
 function onErrorHandler(error, request, response) {
+  console.log("error", error);
+  if (error instanceof ValidationError) {
+    return response.status(error.statusCode).json(error);
+  }
+
   const publicErrorObject = new InternalServerError({
     statusCode: error.statusCode,
     cause: error,
