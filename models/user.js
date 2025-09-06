0187ce2a7eeba16b5eca02fe53cca58b744d55d0
Gabriel Porto <70228491+p0rto@users.noreply.github.com>
2025-09-06 12:55:55 +0000

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,35 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { NotFoundError, ValidationError } from "infra/errors.js";
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+        SELECT
+          *
+        FROM 
+          users
+        WHERE
+          LOWER(username) = LOWER($1)
+        LIMIT
+          1
+        ;`,
+      values: [username],
+    });
+
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
 
 async function create(userInputValues) {
   await validateUniqueUser(userInputValues.email, userInputValues.username);
@@ -53,6 +83,7 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
